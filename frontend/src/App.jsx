96560882f52a79908467d9a14074dcedeaa75e66
utshasamanta <utshalm@gmail.com>
2025-06-25 15:53:21 -0600

    added react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,15 @@
 import { Search } from "lucide-react"
+import { Link, Routes, Route } from "react-router-dom"
+import Home from "./Components/Home"
 
 const App = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-50">
-      <div className="max-w-4xl text-center flex flex-col justify-center items-center font-inter">
-
-        <div>
-          <h1 className="font-bold text-3xl md:text-5xl text-stone-800">Welcome to Scene Search</h1>
-          <h2 className="font-medium text-sm md:text-2xl text-stone-500 mt-2">Relive your memories with just a search</h2>
-        </div>
-
-        <div className="flex flex-col gap-2 sm:flex-row md:gap-5 mt-2">
-          <button className="btn bg-stone-700 hover:bg-stone-800 text-white transition-all duration-400">Find a memory</button>
-          <button className="btn btn-ghost border border-stone-700 hover:bg-stone-700 hover:text-white transition-all duration-400">Store a memory</button>
-        </div>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/upload" element={<div>Upload Media</div>}></Route>
+        <Route path="/search" element={<div>Search Media</div>}></Route>
+      </Routes>
         
         {/* <form className="mt-3" action="https://www.youtube.com/">
           <div className="form-control">
@@ -30,7 +26,6 @@ const App = () => {
             </div>
           </div>
         </form> */}
-      </div>
     </div>
   )
 }
---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+const Home = () => {
+    return (       
+        <div className="max-w-4xl text-center flex flex-col justify-center items-center font-inter">
+
+            <div>
+            <h1 className="font-bold text-3xl md:text-5xl text-stone-800">Welcome to Scene Search</h1>
+            <h2 className="font-medium text-sm md:text-2xl text-stone-500 mt-2">Relive your memories with just a search</h2>
+            </div>
+
+            <div className="flex flex-col gap-2 sm:flex-row md:gap-5 mt-2">
+            <Link to="/upload" className="btn bg-stone-700 hover:bg-stone-800 text-white transition-all duration-400">Find a memory</Link>
+            <Link to="/search" className="btn btn-ghost border border-stone-700 hover:bg-stone-700 hover:text-white transition-all duration-400">Store a memory</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home
